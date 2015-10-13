fb5a85a914fe38668ea0a43391ab316bd45b08cf
basiabodurov <basiabodurov@gmail.com>
2015-10-13 11:10:23 -0700

    Sass added

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,18 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var gulp        = require('gulp');
-var connect = require('gulp-connect');
+'use strict';
+var gulp = require('gulp');// Load Gulp!
 var browserSync = require('browser-sync').create();
-
-gulp.task('compress-js', function() { 
-	gulp.src('js/*.js').pipe(concat('main.min.js')).pipe(uglify()).pipe(gulp.dest('build/js'))
+var sass = require('gulp-sass');
+gulp.task('scss', function () {
+	gulp.src('./scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./css'));
 });
-
-gulp.task('message', function(){
-	console.log('All done!')
-});
-
-gulp.task('all-the-tasks', ['compress-js', 'message'])
-
-gulp.task('compress-css', function() { 
-	gulp.src('css/*.css').pipe(concat('main.css')).pipe(gulp.dest('build/css'))
-});
-
-gulp.task('watch', function (){
-	gulp.watch('js/*.js', ['compress-js']);
-	gulp.watch('css/*.css', ['compress-css']);
-
-});
-
-gulp.task('connect', function() { 
-	connect.server();
-});
-
-gulp.task('browser-sync', ['compress-css', 'compress-js'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-gulp.watch(["index.html","css/*.css","js/*.js"]).on('change', browserSync.reload);
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch('./scss/**/*.scss', ['scss']);
+	gulp.watch(["*/**/*.html","css/*.css","js/*.js"]).on('change', browserSync.reload);
 });
