ca41aac04ce2a31f361f17075112e636427b3606
basiabodurov <basiabodurov@gmail.com>
2015-10-14 16:35:55 -0700

    Mobile version + autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@
 var gulp = require('gulp');// Load Gulp!
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 gulp.task('scss', function () {
 	gulp.src('./scss/**/*.scss')
 	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
 	.pipe(gulp.dest('./css'));
 });
 gulp.task('browser-sync', function() {
@@ -13,6 +17,7 @@ gulp.task('browser-sync', function() {
 			baseDir: "./"
 		}
 	});
+
 	gulp.watch('./scss/**/*.scss', ['scss']);
 	gulp.watch(["*/**/*.html","css/*.css","js/*.js"]).on('change', browserSync.reload);
 });
--- js/main.js
+++ js/main.js
@@ -1,21 +1,50 @@
-var now = new Date();
-
-var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-
-var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
-
-function fourdigits(number)	{
-	return (number < 1000) ? number + 1900 : number;
-								}
-today =  days[now.getDay()] + ", " +
-         months[now.getMonth()] + " " +
-         date + ", " +
-         (fourdigits(now.getYear())) ;
-
-document.write(today);
-
-function goodbye() {
-	console.log('goodbye');
+$(function() {
+var albumData,
+albumItems,
+artistName,
+instagramUrl,
+$albumList = $('.album-list');
+$('#album-search').on('submit', function(event) {
+event.preventDefault();
+$albumList.empty();
+albumData, albumItems = '',
+artistName = $('#artist-name').val().replace(/ /g, '+'),
+instagramUrl = 'https://api.instagram.com/v1/tags/'+artistName+'/media/recent?client_id=fed3ea7d36644c4993d81a12621c74c8';
+$.ajax({
+method: 'GET',
+url: instagramUrl,
+dataType: 'jsonp'
+})
+.done(function(results) {
+albumData = results.data;
+if ( albumData.length !== 0 ) {
+$.each(albumData, function(key, value) {
+albumItems += '<li>';
+albumItems += '<div class="pics">';
+albumItems += '<img src="' + value.images.low_resolution.url + '" />';
+albumItems += '<div class="pic-container">';
+albumItems += '<div class="profile">';
+albumItems += '<img src="' + value.caption.from.profile_picture + '" />';
+albumItems += '<div class="img-bottom">';
+albumItems += '<p>' + value.caption.from.username + '</p>';
+albumItems += '<i class="fa fa-heart"></i>' + value.likes.count + '';
+albumItems += '<i class="fa fa-wechat"></i> ' +value.comments.count;
+albumItems += '</div>';
+albumItems += '</div>';
+albumItems += '</div>';
+albumItems += '</div>';
+albumItems += '</li>';
+});
+} else {
+albumItems += '<p style="margin-top: 18px;">Sorry, artist not found.</p>';
 }
+$albumList.append(albumItems);
+})
+    // and if it fails...
+    .fail(function() {
+       $albumList.append('<li>Sorry! There was a problem, please try again.</li>');
+    });
+ });
+});
+
+
